2c5e1c394698f1ab1b6788712a3e15ba84ce9ded
bta99 <tuananhvip1099@gmail.com>
2023-05-25 02:09:59 +0700

    fix loi index.js

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const socketIO = require("socket.io");
 let cors = require("cors"); //tránh lỗi CORS
 const app = express();
 const server = require("http").Server(app);
-app.use(cors());
+// app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://nodejs-socket-2023.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "Content-Type",
-    "Authorization"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://nodejs-socket-2023.vercel.app"
+//   );
+//   // res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+//   // res.setHeader(
+//   //   "Access-Control-Allow-Methods",
+//   //   "Content-Type",
+//   //   "Authorization"
+//   // );
+//   next();
+// });
 
 app.get("/a", cors(), (req, res) => {
   return res.send("Abcxyz");
